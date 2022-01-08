942fac347a77e34d21a9429d9e38c3b467595bbc
Sherwin T Zuniga Jr <93949031+sherwintzunigajr@users.noreply.github.com>
2022-01-08 15:05:34 -0700

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -2,7 +2,6 @@
 // AUTHOR: Sherwin T Zuniga Jr
 // FILENAME: Zuniga_Sherwin_Hero.java
 // SPECIFICATION: Dungeon Crawler OOP Polymorphism - Hero Abstract Class 
-// FOR: CSE 205- assignment3 - Justin Selgrad
 // TIME SPENT: 30 minutes
 ---------------------------------------*/
 
