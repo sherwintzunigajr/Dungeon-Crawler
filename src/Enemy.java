b63f986acf470e4d029ecd1b5eea5db8497eab84
Sherwin T Zuniga Jr <93949031+sherwintzunigajr@users.noreply.github.com>
2022-01-08 15:05:05 -0700

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,7 +2,6 @@
 // AUTHOR: Sherwin T Zuniga Jr
 // FILENAME: Zuniga_Sherwin_Enemy.java
 // SPECIFICATION: Dungeon Crawler OOP Polymorphism - Enemy Abstract Class
-// FOR: CSE 205- assignment3 - Justin Selgrad
 // TIME SPENT: 30 minutes
 ---------------------------------------*/
 
